d78d61052f1a6ef0fdeb54d6912d1654b4fedef1
Milos A <milos.andjelkovic@redstork-solutions.com>
2023-02-03 10:00:51 +0100

    TODO VALEURS_GENEREES_PAR_PAYS_PER_CLUSTER

--- src/solver/simulation/sim_structure_donnees.h
+++ src/solver/simulation/sim_structure_donnees.h
@@ -68,7 +68,7 @@ typedef struct
 
 typedef struct
 {
-    std::map<int, VALEURS_GENEREES_PAR_PAYS> GenValuesPerAreaPerCluster; 
+    std::map<int, VALEURS_GENEREES_PAR_PAYS> GenValuesPerAreaPerCluster; // TODO Milos: is this structure really needed per each cluster
 } VALEURS_GENEREES_PAR_PAYS_PER_CLUSTER;
 
 typedef struct
