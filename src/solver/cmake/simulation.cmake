75a9a6097b184e706faf9bd95120e162c141aa13
Milos A <milos.andjelkovic@redstork-solutions.com>
2023-03-01 21:36:04 +0100

    change file names for hydro-final-reservoir-level

--- src/solver/cmake/simulation.cmake
+++ src/solver/cmake/simulation.cmake
@@ -22,8 +22,8 @@ set(SRC_SIMULATION
 		simulation/timeseries-numbers.cpp
 		simulation/apply-scenario.h
 		simulation/apply-scenario.cpp
-		simulation/hydro-final-reservoir-level-pre-checks.h
-		simulation/hydro-final-reservoir-level-pre-checks.cpp
+		simulation/hydro-final-reservoir-level-functions.h
+		simulation/hydro-final-reservoir-level-functions.cpp
 		
 
 		# Solver
